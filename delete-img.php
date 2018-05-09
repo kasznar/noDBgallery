5ac02d4b8685e58bfa2bc745564efab4ade3f06a
Kasznár Dávid <kasznardavid8@gmail.com>
2018-05-10 00:27:40 +0200

    preventing deleting outside the galleries directory

--- delete-img.php
+++ delete-img.php
@@ -5,11 +5,16 @@ if (!isset($_SESSION['loggedIn']) || $_SESSION['loggedIn'] == false) {
 }
 
 $this_img_path = trim($_POST['img_to_delete']);
+
+//prevent deleting anything outside the galleryes folder
+$this_img_path_array =  explode("galleries", $this_img_path);
+$this_img_path = "galleries".$this_img_path_array[1];
+
+//delete thumb image
 unlink($this_img_path);
 
+//delete web size image
 $path_arr = explode("thumb-img", $this_img_path);
 unlink($path_arr[0]."web-size-img".$path_arr[1]);
-
-
 
 ?>
